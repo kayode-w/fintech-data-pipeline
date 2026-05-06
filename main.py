1d32d952a468467159ca0aa1c2cf69e77569e782
kayodew <olukayode.williams@outlook.com>
2026-05-06 23:03:27 +0100

    added app_events & wallet anomalies to dup dealer.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from dealer import generate_users, generate_transactions, app_events, generate_wallet_balance, inject_transactions_anomalies, inject_users_anomalies, save_to_csv
+from dealer import generate_users, generate_transactions, app_events, generate_wallet_balance, inject_transactions_anomalies, inject_users_anomalies, inject_app_event_anomalies, inject_wallet_anomalies, save_to_csv
 
 
 
@@ -9,5 +9,12 @@ wallet_balance = generate_wallet_balance(users)
 
 users_2 = inject_users_anomalies(users)
 transactions_2 = inject_transactions_anomalies(transactions)
+# print(transactions_2.loc[transactions_2['amount'].apply(lambda x: isinstance(x, str()))].head())
+events_2 = inject_app_event_anomalies(users, events)
+wallet_balance_2 = inject_wallet_anomalies(wallet_balance)
 
-print(transactions_2.loc[transactions_2['amount'].apply(lambda x: isinstance(x, str()))].head())
+
+save_to_csv(users_2, 'users.csv')
+save_to_csv(transactions_2, 'transactions.csv')
+save_to_csv(events_2, 'app_events.csv')
+save_to_csv(wallet_balance_2, 'wallet_balances.csv')
